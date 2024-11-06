4c3a4c9257d1af92cbc4a8e2736151d2008b93be
janani.manimozhi <janani.manimozhi@accenture.com>
2024-11-06 10:45:23 +0530

    alert button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  // Function to trigger the alert
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Alert Example</h1>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
